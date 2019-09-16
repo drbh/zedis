920066f36a57484a0e907ee8e44b1f1f9722a921
drbh <david.richard.holtz@gmail.com>
2019-09-16 16:12:07 -0400

    update bin homebrew

--- zedis.rb
+++ zedis.rb
@@ -1,7 +1,7 @@
 class Zedis < Formula
   desc "A tiny key value datastore"
   homepage "https://github.com/drbh/zedis"
-  url "https://github.com/drbh/zedis/archive/v0.0001.tar.gz"
+  url "https://github.com/drbh/zedis/releases/download/v0.0001/zedis"
   sha256 "e66cdf097e9b902ddbd2bd5f1f584dd860c51c8ca6d3f9bb539deeb4633c22a4"
   version "0.0.1"
 
