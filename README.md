b796eb6e0683095e41d8cf8b3516508042ffe568
drbh <david.richard.holtz@gmail.com>
2019-09-16 10:33:36 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# ZEDIS <img src="https://img.shields.io/github/stars/drbh/zedis.svg" />
+
+A lightweight zero mq and sled based replacement for Redis. <50 LOC focused on a portable corss language simple storage system. No need to run a full service.
+
+Zedis is opinionated and limited. With ZEDIS you can only READ and WRITE key values pairs. All writes overwrite past values. 
+
+Really Fast reads `< 250µs` for `~2 MB` json payload.   
+
+Pretty Fast writes `< 18ms` for `~2 MB` json payload.  
+
+## Read/Write from Python
+```python
+import zmq
+import json
+
+port = "5555"
+
+context = zmq.Context()
+socket = context.socket(zmq.REQ)
+socket.connect("tcp://localhost:%s" % port)
+
+socket.send("SET david richard blyn holtz")
+socket.recv()
+# 'done.'
+
+socket.send("GET david")
+socket.recv()
+# 'richard blyn holtz'
+
+jsonblob = json.dumps({"example": "lorem ipsum"*2048})
+socket.send("SET js "+jsonblob)
+socket.recv()
+# 'done.'
+
+socket.send("GET js")
+json.loads(socket.recv())
+# {u'exmple': u'dasfsa'}
+```
+
+## Limited
---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use sled::Db;
+use std::str;
+use std::time::Instant;
+
+
+fn get(t: sled::Db, key: &[u8]) -> std::option::Option<sled::IVec> {
+	let result = t.get(key).unwrap();
+	result
+}
+fn set(t: sled::Db, key: &[u8], value: &[u8]) -> std::option::Option<sled::IVec> {
+	let result = t.insert(key, value).unwrap();
+	result
+}
+
+fn main() {
+    let context = zmq::Context::new();
+    let responder = context.socket(zmq::REP).unwrap();
+    assert!(responder.bind("tcp://*:5555").is_ok());
+    let mut msg = zmq::Message::new();
+	let t = Db::open("my_db").unwrap();
+
+    loop {
+        responder.recv(&mut msg, 0).unwrap();
+        let msg_text = msg.as_str().unwrap();
+        let start = Instant::now();
+        let split = msg_text.split_whitespace();
+        let vec: Vec<&str> = split.collect();
+        if vec[0] == "GET" {
+        	let r = get(t.clone(), vec[1].as_bytes());
+        	let byts = r.unwrap();
+        	let s = String::from_utf8(byts.to_vec()).expect("Found invalid UTF-8");
+        	responder.send(s.as_str(), 0).unwrap();
+        }
+        if vec[0] == "SET" {
+        	let joined = vec[2..vec.len()].to_vec().join(" ");
+        	let _r = set(t.clone(), vec[1].as_bytes(), joined.as_bytes());
+        	responder.send("done.", 0).unwrap();
+        }
+        let _duration = start.elapsed();
+        // println!("{} {:#?}", msg_text, _duration);
+        println!("{:#?}", _duration);
+    }
+}
