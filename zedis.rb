2301be371eb3a43956d7e979e42a6496cd299642
drbh <david.richard.holtz@gmail.com>
2019-09-16 16:10:11 -0400

    update sha sig

--- zedis.rb
+++ zedis.rb
@@ -2,7 +2,7 @@ class Zedis < Formula
   desc "A tiny key value datastore"
   homepage "https://github.com/drbh/zedis"
   url "https://github.com/drbh/zedis/archive/v0.0001.tar.gz"
-  sha256 "2f65b56d61dda3408502084e9e1692c26f7dff09fe3a6fed80606a21aeeb5c51"
+  sha256 "e66cdf097e9b902ddbd2bd5f1f584dd860c51c8ca6d3f9bb539deeb4633c22a4"
   version "0.0.1"
 
   bottle :unneeded
