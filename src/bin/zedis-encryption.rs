c8e894a46e28d1aa77ca6a4446797c371c0b1cd0
David Holtz <david.richard.holtz@gmail.com>
2019-09-25 02:31:45 -0400

    switch from ring to sodiumoxide

--- src/bin/zedis-encryption.rs
+++ src/bin/zedis-encryption.rs
@@ -6,11 +6,7 @@ extern crate rustyline;
 use rustyline::error::ReadlineError;
 use rustyline::Editor;
 
-extern crate ring;
-
-use ring::aead::*;
-use ring::pbkdf2::*;
-use ring::rand::SystemRandom;
+use sodiumoxide::crypto::aead;
 
 fn execute(socket: &zmq::Socket, cmd: String) -> String {
     let mut msg = zmq::Message::new();
@@ -18,7 +14,7 @@ fn execute(socket: &zmq::Socket, cmd: String) -> String {
     socket.recv(&mut msg, 0).unwrap();
     let msg_text = msg.as_str().unwrap();
     // println!("{}", msg_text);
-    msg_text
+    msg_text.to_string()
 }
 
 fn main() {
@@ -33,63 +29,15 @@ fn main() {
     let address = format!("tcp://127.0.0.1:{}", port);
     socket.connect(&address).unwrap();
 
-    let res = execute(&socket, "keys");
+    let k = aead::gen_key();
+    let n = aead::gen_nonce();
+    let m = b"david holtz";
+    let ad = b"Some additional data";
 
-    // The password will be used to generate a key
-    let password = b"nice password";
+    let c = aead::seal(m, Some(ad), &n, &k);
+    let m2 = aead::open(&c, Some(ad), &n, &k).unwrap();
 
-    // Usually the salt has some random data and something that relates to the user
-    // like an username
-    let salt = [0, 1, 2, 3, 4, 5, 6, 7];
+    let y = String::from_utf8(m2.clone());
 
-    // Keys are sent as &[T] and must have 32 bytes
-    let mut key = [0; 32];
-    derive(&HMAC_SHA256, 100, &salt, &password[..], &mut key);
-
-    // Your private data
-    let content = b"content to encrypt".to_vec();
-    println!("Content to encrypt's size {}", content.len());
-
-    // Additional data that you would like to send and it would not be encrypted but it would
-    // be signed
-    let additional_data: [u8; 0] = [];
-
-    // Ring uses the same input variable as output
-    let mut in_out = content.clone();
-
-    // The input/output variable need some space for a suffix
-    println!("Tag len {}", CHACHA20_POLY1305.tag_len());
-    for _ in 0..CHACHA20_POLY1305.tag_len() {
-        in_out.push(0);
-    }
-
-    // Opening key used to decrypt data
-    let opening_key = OpeningKey::new(&CHACHA20_POLY1305, &key).unwrap();
-
-    // Sealing key used to encrypt data
-    let sealing_key = SealingKey::new(&CHACHA20_POLY1305, &key).unwrap();
-
-    // Random data must be used only once per encryption
-    let mut nonce = vec![0; 12];
-
-    // Fill nonce with random data
-    let rand = SystemRandom::new();
-    rand.fill(&mut nonce).unwrap();
-
-    // Encrypt data into in_out variable
-    let output_size = seal_in_place(
-        &sealing_key,
-        &nonce,
-        &additional_data,
-        &mut in_out,
-        CHACHA20_POLY1305.tag_len(),
-    )
-    .unwrap();
-
-    println!("Encrypted data's size {}", output_size);
-
-    let decrypted_data =
-        open_in_place(&opening_key, &nonce, &additional_data, 0, &mut in_out).unwrap();
-
-    println!("{:?}", String::from_utf8(decrypted_data.to_vec()).unwrap());
+    println!("{:?}", y);
 }
